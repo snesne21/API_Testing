ed9487c2da1b1e1d314f723c31c18d7d9e9b4df4
Sneha Lingaiah <lingaiah.sneha@gmail.com>
2022-06-23 14:42:28 -0700

    changes to test_get.py

--- test_get.py
+++ test_get.py
@@ -1,12 +1,19 @@
+
 import requests
+from assertpy.assertpy import assert_that
+from config import BASE_URI
 
 def test_get_request():
-  response = requests.get("https://1ryu4whyek.execute-api.us-west-2.amazonaws.com/dev/skus")
-  assert (response.status_code == 200), "Status code is not 200 OK. Found this instead:"\
-  +str(response.status_code)
+  response = requests.get(BASE_URI)
+  assert_that(response.status_code).is_equal_to(requests.codes.ok)
+  response_text = response.json()
+  skus = [item['sku'] for item in response_text]
+  assert_that(skus).contains('berliner2')
+  '''
   for sku in response.json()['data']:
       if sku['id'] == 'berliner1':
         assert record['price'] == "2.99",\
               "Data does not match! Expected : 2.99, but found : " + str(record['price'])
         assert record['description'] == "Jelly donut",\
               "Data does not match! Expected : Jelly donut, but found : " + str(record['description'])
+  '''
