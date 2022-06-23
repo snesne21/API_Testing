139f6ae679560e43883f6bcde39681f11e4224d9
Sneha Lingaiah <lingaiah.sneha@gmail.com>
2022-06-23 15:16:27 -0700

    saving right file

--- test_delete.py
+++ test_delete.py
@@ -1,17 +1,17 @@
 
 import requests
-from uuid import uuid4
 import json
 from assertpy.assertpy import assert_that
 from config import BASE_URI
+import test_post.py
 
 
-def test_created_person_can_be_deleted():
-    persons_last_name = create_new_person()
+def test_created_item_can_be_deleted():
+    item_sku = create_new_sku()
 
-    peoples = requests.get(BASE_URI).json()
-    newly_created_user = search_created_user_in(peoples, persons_last_name)[0]
+    items = requests.get(BASE_URI).json()
+    newly_created_sku = search_created_sku_in(items, item_sku)[0]
 
-    delete_url = f'{BASE_URI}/{newly_created_user["person_id"]}'
+    delete_url = f'{BASE_URI}/{newly_created_sku["sku"]}'
     response = requests.delete(delete_url)
     assert_that(response.status_code).is_equal_to(requests.codes.ok)
